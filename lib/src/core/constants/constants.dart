d477387419f17c53ad5651fcfc3530eeae3116c4
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-05-28 20:18:37 -0400

    Added get article method to repository implementation

--- lib/src/core/constants/constants.dart
+++ lib/src/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsApiBaseUrl = 'https://newsapi.org/v2';
+const String newsApiKey = '029f0c066a614bcfac2c497c57eb7aae';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
---
+++ lib/src/data/repository/article_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:news_app/src/core/constants/constants.dart';
+import 'package:news_app/src/core/resources/data_state.dart';
+import 'package:news_app/src/data/sources/remote/news_api_service.dart';
+import 'package:news_app/src/domain/repository/article_repository.dart';
+import 'package:news_app/src/data/models/article.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(this._newsApiService);
+
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsApiKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioError(
+              error: httpResponse.response.statusMessage,
+              response: httpResponse.response,
+              type: DioErrorType.badResponse,
+              requestOptions: httpResponse.response.requestOptions),
+        );
+      }
+    } on DioError catch (error) {
+      return DataFailed(error);
+    }
+  }
+}
